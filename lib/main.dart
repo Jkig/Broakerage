3fa82ff0101e87156b2a23fda4b322d78f3f7a5b
Jkig <78281847+Jkig@users.noreply.github.com>
2021-02-19 20:34:35 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/full_stock.dart';
+import 'pages/loading.dart';
+import 'pages/change_cash.dart';
+import 'pages/change_cash_submit.dart';
+import 'pages/profile.dart';
+import 'pages/search.dart';
+import 'pages/trade.dart';
+import 'pages/trade_confirm.dart';
+
+//import 'services/globals.dart' as globals;
+
+void main() async {
+
+  runApp(MaterialApp(
+    //home: Home(),
+    theme: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.lightBlue[800],
+      accentColor: Colors.cyan[600],
+      ),
+
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/full_stock': (context) => FullStock(),
+      '/change_cash': (context) => ChangeCash(),
+      '/change_cash_submit': (context) => ChangeCashSumbit(),
+      '/profile': (context) => Profile(),
+      '/search': (context) => Search(),
+      '/trade': (context) => Trade(),//make widgets
+      '/trade_confirm': (context) => TradeConfirm(),//make widgets
+
+      //add counters here? to work better?
+    },
+  ));
+}
