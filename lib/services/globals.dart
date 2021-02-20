72a02ef9ccbc61b3eb5ca159552647029479fb22
Jkig <78281847+Jkig@users.noreply.github.com>
2021-02-19 20:30:25 -0500

    Delete lib directory

--- lib/services/globals.dart
+++
@@ -1,3 +0,0 @@
-library my_prj.globals;
-
-Map quant1 = {'cash' :  9904.23,'AAPL' : 0.0, 'TSLA' : 0.0, 'GOOGL' : 3.0, 'SPY' : 0.0, 'INTC' : 0.0, 'DBX' : 0.0, 'FB' : 0.0, 'AMZN' : 0.0, 'PLTR' : 0.0, 'bitcoin' : 1.35 ,'ethereum' : 9.0, 'polkadot' : 668.1};// use for apple
